42e12a61342c7bb26e12a8a2748602765195c68c
Paulljc <reborn_cheng@126.com>
2019-06-12 16:22:59 +0800

    complete code

---
+++ multiplication_table/index.js
@@ -0,0 +1,7 @@
+for (let n = 1; n <= 9; n++) {
+  for (let m = 1; m < n + 1; m++) {
+    let result = m * n;
+    document.write(m + "*" + n + "=" + result + " ");
+  }
+  document.write("<br/>");
+}
---
+++ number/index.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 == 0) {
+    document.write(i + "是偶数。");
+    document.write("<br/>");
+  } else {
+    document.write(i + "是奇数。");
+    document.write("<br/>");
+  }
+}
